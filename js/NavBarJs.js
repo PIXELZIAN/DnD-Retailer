a07d2c8130bc969d02f5d5013b9702f6aea84e88
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-04-04 22:38:52 +0100

    added design on the to make it fit on the navbar

--- js/NavBarJs.js
+++ js/NavBarJs.js
@@ -1,7 +1,7 @@
 let usernames = localStorage.username;
 let currentAccount1 = localStorage.currentAccounts;
 if(currentAccount1 > -1){
-    document.getElementById("username").innerHTML = "<li class='nav-item dropdown'> <a class='nav-link dropdown-toggle' id='navbarDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> "+ usernames +" </a> <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'> <a type='button' onclick='LogOutAccount()' class='dropdown-item'>Log Out</a> </div> </li>";
+    document.getElementById("username").innerHTML = "<li class='nav-item dropdown'> <a class='nav-link text-dark bg-white border border-light rounded dropdown-toggle' id='navbarDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> "+ usernames +" </a> <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'> <a type='button' onclick='LogOutAccount()' class='dropdown-item'>Log Out</a> </div> </li>";
 }
 
 function LogOutAccount() {
