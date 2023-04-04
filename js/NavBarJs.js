8cb9affe063a92f7fe179a3b1b15ab7b44d601c6
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-04-04 22:25:52 +0100

    add a log out button to the navbar

--- js/NavBarJs.js
+++ js/NavBarJs.js
@@ -1,5 +1,11 @@
-let username1 = localStorage.username;
-let currentAccount = localStorage.currentAccounts;
-if(currentAccount > -1){
-    document.getElementById("username").innerHTML = username1;
+let usernames = localStorage.username;
+let currentAccount1 = localStorage.currentAccounts;
+if(currentAccount1 > -1){
+    document.getElementById("username").innerHTML = "<li class='nav-item dropdown'> <a class='nav-link dropdown-toggle' id='navbarDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> "+ usernames +" </a> <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'> <a type='button' onclick='LogOutAccount()' class='dropdown-item'>Log Out</a> </div> </li>";
+}
+
+function LogOutAccount() {
+  localStorage.username = ""
+  localStorage.currentAccounts = -1
+  location.href = "HomePage.html";
 }
--- js/SignInJs.js
+++ js/SignInJs.js
@@ -1,4 +1,4 @@
-// emails and passwords for account 
+// emails and passwords for account;
 let emails = ["bob@dnd.ie", "alice@dnd.ie", "john@dnd.ie"];
 let passwords = ["abcdefgh", "01234567", "a2c4e6h8"];
 let username1 = ["Bob", "Alice", "John"];
