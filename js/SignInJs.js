eca173cdef8fcf339bebb8fa55c0e28ec86754b3
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-04-04 23:23:52 +0100

    updated Sign Up page
    SignUpPage.html has all the is done
    
    SignInJs.js just need to be finished off by add data to the local storage and  check if all inputs are filled out

--- js/SignInJs.js
+++ js/SignInJs.js
@@ -6,11 +6,11 @@ let currentAccount = -1;
 
 // check if account is valid
 function validateAccount(){
-    let email1 = document.getElementById("userEmail").value;
-    let password1 = document.getElementById("userPassword").value;
+    let emailSignIn = document.getElementById("userEmail").value;
+    let passwordSignIn  = document.getElementById("userPassword").value;
 
     for(let i = 0; i < emails.length; i++){
-        if(email1 == emails[i] && password1 == passwords[i]){
+        if(emailSignIn == emails[i] && passwordSignIn == passwords[i]){
             currentAccount = i;
             localStorage.currentAccounts = currentAccount;
             localStorage.username = username1[i];
@@ -18,4 +18,44 @@ function validateAccount(){
         }
         else{}
     }
+}
+
+function newAccount(){
+    let emailSignUp = document.getElementById("userEmail").value;
+    let username2 = document.getElementById("usernameSignUp").value;
+    let passwordSignUp1 = document.getElementById("userPassword1").value;
+    let passwordSignUp2 = document.getElementById("userPassword2").value;
+
+     //check if email is valid
+     var at = "@";
+     var dot = ".";
+     let emailAt = emailSignUp.indexOf(at);
+     let emailDot = emailSignUp.indexOf(dot);
+     if( emailAt == -1 || emailDot == -1){
+         alert("You have entered an invalid email address!")
+         return (false)
+     }
+     else{
+         return (true)
+     }
+
+    //if username has 8 or more characters
+    if (username2.length < 8) {
+        alert("Name must have 8 characters");
+        return false;
+    }
+
+    //password
+    //if password has 8 or more characters
+    if (passwordSignUp1.length < 8) {
+        alert("Password must have 8 characters");
+        return false;
+    }
+    //if password1 and password2 is the same
+    if (password1 == passwordSignUp2) {
+        return true;
+    } else{
+        alert("Password must be the same");
+        return false;
+    }	   
 }
