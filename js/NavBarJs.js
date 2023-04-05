e4db9f7926079d138d0e924ee7722f5e09ff6aae
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-04-05 15:47:46 +0100

    sign up page is done
    you can now generate one new account

--- js/NavBarJs.js
+++ js/NavBarJs.js
@@ -1,11 +1,11 @@
 let usernames = localStorage.username;
-let currentAccount1 = localStorage.currentAccounts;
-if(currentAccount1 > -1){
+let currentAccountNum1 = localStorage.currentAccountsNum;
+if(currentAccountNum1 > -1){
     document.getElementById("username").innerHTML = "<li class='nav-item dropdown'> <a class='nav-link text-dark bg-white border border-light rounded dropdown-toggle' id='navbarDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'> "+ usernames +" </a> <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'> <a type='button' onclick='LogOutAccount()' class='dropdown-item'>Log Out</a> </div> </li>";
 }
 
 function LogOutAccount() {
   localStorage.username = ""
-  localStorage.currentAccounts = -1
+  localStorage.currentAccountsNum = -1
   location.href = "HomePage.html";
 }
--- js/SignInJs.js
+++ js/SignInJs.js
@@ -2,60 +2,72 @@
 let emails = ["bob@dnd.ie", "alice@dnd.ie", "john@dnd.ie"];
 let passwords = ["abcdefgh", "01234567", "a2c4e6h8"];
 let username1 = ["Bob", "Alice", "John"];
-let currentAccount = -1;
+let currentAccountNum = -1;
+
+// if new account made and add to the array
+emails.push(localStorage.NewUserEmail);
+passwords.push(localStorage.Newpassword);
+username1.push(localStorage.NewUserName);
 
 // check if account is valid
 function validateAccount(){
     let emailSignIn = document.getElementById("userEmail").value;
     let passwordSignIn  = document.getElementById("userPassword").value;
+    
 
     for(let i = 0; i < emails.length; i++){
         if(emailSignIn == emails[i] && passwordSignIn == passwords[i]){
-            currentAccount = i;
-            localStorage.currentAccounts = currentAccount;
+            currentAccountNum = i;
+            localStorage.currentAccountsNum = currentAccountNum;
             localStorage.username = username1[i];
             location.href = "HomePage.html";
         }
-        else{}
+       else{}
     }
 }
 
 function newAccount(){
+    let isValid = 0 ;
     let emailSignUp = document.getElementById("userEmail").value;
-    let username2 = document.getElementById("usernameSignUp").value;
-    let passwordSignUp1 = document.getElementById("userPassword1").value;
-    let passwordSignUp2 = document.getElementById("userPassword2").value;
-
      //check if email is valid
      var at = "@";
      var dot = ".";
      let emailAt = emailSignUp.indexOf(at);
      let emailDot = emailSignUp.indexOf(dot);
      if( emailAt == -1 || emailDot == -1){
-         alert("You have entered an invalid email address!")
-         return (false)
+         alert("You have entered an invalid email address!");
      }
      else{
-         return (true)
+        isValid = isValid + 1;
      }
 
+    let username2 = document.getElementById("usernameSignUp").value;
     //if username has 8 or more characters
-    if (username2.length < 8) {
-        alert("Name must have 8 characters");
-        return false;
-    }
+    if (username2.length < 3) {
+        alert("Name must have 3 characters");
+    } else { isValid = isValid + 1;}
 
+    let passwordSignUp1 = document.getElementById("userPassword1").value;
+    let passwordSignUp2 = document.getElementById("userPassword2").value;
     //password
     //if password has 8 or more characters
     if (passwordSignUp1.length < 8) {
         alert("Password must have 8 characters");
-        return false;
-    }
+    } else { isValid = isValid + 1;}
     //if password1 and password2 is the same
-    if (password1 == passwordSignUp2) {
-        return true;
+    if (passwordSignUp1 == passwordSignUp2) {
+        isValid = isValid + 1;
     } else{
         alert("Password must be the same");
-        return false;
-    }	   
+    }	
+    
+    // check if all validation is true
+    if(isValid == 4){
+        localStorage.NewUserName = username2;
+        localStorage.NewUserEmail = emailSignUp;
+        localStorage.Newpassword= passwordSignUp1;
+
+        location.href = "SignInPage.html";        
+        return true;
+    }    
 }
