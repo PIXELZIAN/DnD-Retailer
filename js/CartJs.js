f04e39d3a3752b009f3c0d3cfe889c463d42d7bb
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-04-18 21:52:25 +0100

    fix CartJs and shop Javascript to its own js file
    cart page now displays 0 not NaN

--- js/CartJs.js
+++ js/CartJs.js
@@ -1,12 +1,21 @@
 function read(){
     let diceSet = sessionStorage.qty1; 
     let diceMat = sessionStorage.qty2; 
-    let essentialsKit = sessionStorage.qty3; 
+    let essentialsKit = sessionStorage.qty3;
     let diceBag = sessionStorage.qty4;
-    
+
+    // check if variables not undefined
+    if(typeof diceSet === 'undefined')
+    {diceSet = 0;}    
+    if(typeof diceMat === 'undefined')
+    {diceMat = 0;}    
+    if(typeof essentialsKit === 'undefined')
+    {essentialsKit = 0;} 
+    if(typeof diceBag === 'undefined')
+    {diceBag = 0;}       
+
     //alert("Order details extracted from web storage and displayed in table");
     // get quantity for each Products
-
     document.getElementById("qty1").textContent = diceSet;
     document.getElementById("qty2").textContent = diceMat;
     document.getElementById("qty3").textContent = essentialsKit;
@@ -18,12 +27,9 @@ function read(){
     document.getElementById("total3").textContent = essentialsKit * 19.99;
     document.getElementById("total4").textContent = diceBag * 11.99;
 
-    /*diceSet = parseInt(diceSet);
-    diceMat = parseInt(diceMat);
-    essentialsKit = parseInt(essentialsKit);
-    diceBag = parseInt(diceBag);*/
-
     // get total cost
     document.getElementById("total").textContent = ((diceSet * 8.99) + (diceMat * 11.99) + (essentialsKit * 19.99) + (diceBag * 11.99));
 }	
 read();
+
+ 
---
+++ js/ShopJs.js
@@ -0,0 +1,13 @@
+//Quantity of the Products
+function add(quantity){
+    if(quantity == 1){
+        sessionStorage.qty1 = document.getElementById("qty1").value;
+    }else if(quantity == 2){
+        sessionStorage.qty2 = document.getElementById("qty2").value;
+    }else if(quantity == 3){
+        sessionStorage.qty3 = document.getElementById("qty3").value;
+    }else if(quantity == 4){
+        sessionStorage.qty4 = document.getElementById("qty4").value;
+    }
+    alert("Order added to cart");
+}
